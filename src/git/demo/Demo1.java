c82b20d9bbbad5557c348cd4370a13ccdd40cd59
黄靖 <huang3133522@163.com>
2019-12-03 16:46:48 +0800

    提交到本地仓库

---
+++ src/git/demo/Demo1.java
@@ -0,0 +1,11 @@
+package git.demo;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
